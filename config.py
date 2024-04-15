d7d1662eb81207e5a72943a879183491d5a621bf
Anonimous05 <airastolonov05@gmail.com>
2024-04-15 18:15:46 +0600

    Example static files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CORE_DIR: str = os.getcwd()
-
-DATASETS_ROOT: str = os.path.join(CORE_DIR, 'datasets')
-
-STATIC_ROOT: str = os.path.join(CORE_DIR, 'static')
-IMAGES_ROOT: str = os.path.join(STATIC_ROOT, 'images')
-VIDEOS_ROOT: str = os.path.join(STATIC_ROOT, 'videos')
